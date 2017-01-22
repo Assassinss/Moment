163d703bc58f44e91bff513efacb29ae4d8e32e6
Assassinss <931289622@qq.com>
2017-01-22 14:01:42 +0800

    Update about ui

--- app/src/main/java/me/zsj/moment/AboutActivity.java
+++ app/src/main/java/me/zsj/moment/AboutActivity.java
@@ -5,16 +5,17 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /**
  * @author zsj
  */
 
-public class AboutActivity extends AppCompatActivity {
+public class AboutActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,22 +24,20 @@ public class AboutActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        assert getSupportActionBar() != null;
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         TextView version = (TextView) findViewById(R.id.version);
         version.setText(String.format("Version %s", BuildConfig.VERSION_NAME));
 
-        TextView sourceLink = (TextView) findViewById(R.id.source_link);
-        sourceLink.setOnClickListener(v -> {
-            Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.setData(Uri.parse("https://github.com/Assassinss/Moment"));
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
-        });
+        LinearLayout github = (LinearLayout) findViewById(R.id.github);
+        github.setOnClickListener(this);
 
-        RecyclerView sourceList = (RecyclerView) findViewById(R.id.source_list);
-        SourceListAdapter adapter = new SourceListAdapter(this);
-        sourceList.setAdapter(adapter);
+        LinearLayout contacts = (LinearLayout) findViewById(R.id.contacts);
+        contacts.setOnClickListener(this);
+
+        LinearLayout sourceCode = (LinearLayout) findViewById(R.id.source_code);
+        sourceCode.setOnClickListener(this);
     }
 
     @Override
@@ -49,4 +48,23 @@ public class AboutActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.github) {
+            Intent githubIntent = new Intent(Intent.ACTION_VIEW);
+            githubIntent.setData(Uri.parse("https://github.com/Assassinss"));
+            githubIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(githubIntent);
+        } else if (v.getId() == R.id.contacts) {
+            Intent contactsIntent = new Intent(Intent.ACTION_SENDTO);
+            contactsIntent.setData(Uri.parse("mailto:zhu948742@gmail.com"));
+            contactsIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(contactsIntent);
+        } else if (v.getId() == R.id.source_code) {
+            Intent sourceIntent = new Intent(Intent.ACTION_VIEW);
+            sourceIntent.setData(Uri.parse("https://github.com/Assassinss/Moment"));
+            sourceIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(sourceIntent);
+        }
+    }
 }
