b7c5815db225447c9f2cec5b21abbc2903dc0bc8
Assassinss <931289622@qq.com>
2017-03-03 13:16:51 +0800

    Make navigation bar full completely transparent

--- app/src/main/java/me/zsj/moment/PictureActivity.java
+++ app/src/main/java/me/zsj/moment/PictureActivity.java
@@ -60,7 +60,9 @@ public class PictureActivity extends RxAppCompatActivity
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
         getWindow().setStatusBarColor(Color.TRANSPARENT);
         setContentView(R.layout.picture_activity);
 
